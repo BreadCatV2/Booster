b6ed23c246e422368fec9c156a82f8579b9a8257
SamC4r <locoporlosrubiks@gmail.com>
2025-10-15 15:38:04 +0200

    robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "*", disallow: "/api" },
+    ],
     sitemap: "https://www.boostervideos.net/sitemap.xml",
   };
 }
+
